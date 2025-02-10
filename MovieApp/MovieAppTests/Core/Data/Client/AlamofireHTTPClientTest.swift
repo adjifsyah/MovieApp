4252a5bd813ecaac0de43d85cde00f22a1aa1068
adjifsyah <adji.firma>
2025-02-10 23:24:01 +0700

    sdgsd

--- MovieApp/MovieAppTests/Core/Data/Client/AlamofireHTTPClientTest.swift
+++ MovieApp/MovieAppTests/Core/Data/Client/AlamofireHTTPClientTest.swift
@@ -71,7 +71,7 @@ class AlamofireHTTPClientTest: XCTestCase {
                 onError: { error in
                     if let afError = error as? AFError,
                        let underlyingError = afError.underlyingError as NSError? {
-                        XCTAssertEqual(underlyingError.domain, "MockErrorss")
+                        XCTAssertEqual(underlyingError.domain, "MockErrorssss")
                         XCTAssertEqual(underlyingError.code, 100)
                         expectation.fulfill()
                     }
